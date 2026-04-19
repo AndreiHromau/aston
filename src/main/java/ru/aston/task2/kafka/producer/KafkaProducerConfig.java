a02dff95f96c11853fa743565d79628303d375a1
AndreiHromau <Andreihromau@gmail.com>
2026-04-19 19:51:13 +0300

    refactor: KafkaProducerConfig

--- src/main/java/ru/aston/task2/kafka/producer/KafkaProducerConfig.java
+++ src/main/java/ru/aston/task2/kafka/producer/KafkaProducerConfig.java
@@ -22,6 +22,7 @@ public class KafkaProducerConfig {
     ) {
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.ACKS_CONFIG, "0");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         config.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
