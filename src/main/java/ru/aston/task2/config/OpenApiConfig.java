338f85d1f73fdc4d86ddda5b91d8767b6b61e867
AndreiHromau <Andreihromau@gmail.com>
2026-04-25 17:21:48 +0300

    feat: add OpenApiConfig

---
+++ src/main/java/ru/aston/task2/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package ru.aston.task2.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Service API")
+                        .version("v1")
+                        .description("CRUD API для управления пользователями"));
+    }
+}
